206f7b848f12d9c2f3cc9091f0ada0884a94c372
Rob Birch <rbirchtree@gmail.com>
2020-07-21 19:10:13 -0700

    removed comments and console log in file

--- src/components/Accident.js
+++ src/components/Accident.js
@@ -3,7 +3,6 @@ import React from "react";
 const Accident = (props) => {
     const {dateOfAccident, numOfVisitsToRehab ,timeAtRehabInHours, timeDrivingtoRehabPerAVisitRndTrip, timeWithAtty, timeAtAccident, timeRentingACar, timeDoingRehabPerADay, dateRehabIsFinished, hourlyWageForOccupation ,occupation, zipCodeOfAccident, insurance, age, settlementAmt, notes} = props.val.accident
     
-    console.log('props this is it.',props.val.accident.id)
     return (
         <ul >
             <li>{dateRehabIsFinished}</li>
@@ -20,14 +19,4 @@ const Accident = (props) => {
     )
 }
 
-// date of accident,, date rehab is finished, num of visits to rehab
-//  hourly wage for occupation, occupation, settlementmat, notes, insurance
-// zipcode of accident, age
-// INSERT INTO USERDATA (dateOfAccident, numOfVisitsToRehab ,timeAtRehabInHours,
-//      timeDrivingtoRehabPerAVisitRndTrip, 
-//     timeWithAtty, timeAtAccident, timeRentingACar, 
-//     timeDoingRehabPerADay, dateRehabIsFinished, 
-//     hourlyWageForOccupation ,occupation,
-//     zipCodeOfAccident, insurance, age, settlementAmt,notes)
-//     VALUES ('4/14/2018', 16, 1,1,1,2,1,1,'04/14/18',8,'Retired',78634, 'ACME', 83, 5000,'He was hit by a van while parked'); 
 export default Accident;
