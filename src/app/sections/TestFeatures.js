5720e0e30220352f07af8d90203d3c33141a0f79
Andrew <you@example.com>
2020-08-02 10:37:20 -0500

    removed uploadData with api key

---
+++ src/app/sections/TestFeatures.js
@@ -0,0 +1,10 @@
+import React from "react";
+import data from "data.js";
+
+import firebase from "config/firebase";
+
+let db = firebase.firestore();
+
+export default function TestFeatures() {
+  return <div></div>;
+}
