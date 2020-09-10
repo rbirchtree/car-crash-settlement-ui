330c2025df764456579dd89c0dde93f3850b5371
Andrew <huandrew93@gmail.com>
2020-09-09 22:53:19 -0500

    added Accordion to FAQPage (#11)

--- src/app/pages/FAQ.js
+++ src/app/pages/FAQ.js
@@ -1,7 +1,60 @@
 import React from "react";
 import Helmet from "react-helmet";
 
-export default function FAQ() {
+import { withStyles } from "@material-ui/core/styles";
+import MuiAccordion from "@material-ui/core/Accordion";
+import MuiAccordionSummary from "@material-ui/core/AccordionSummary";
+import MuiAccordionDetails from "@material-ui/core/AccordionDetails";
+import Typography from "@material-ui/core/Typography";
+
+const Accordion = withStyles({
+  root: {
+    border: "1px solid rgba(0, 0, 0, .125)",
+    boxShadow: "none",
+    "&:not(:last-child)": {
+      borderBottom: 0,
+    },
+    "&:before": {
+      display: "none",
+    },
+    "&$expanded": {
+      margin: "auto",
+    },
+  },
+  expanded: {},
+})(MuiAccordion);
+
+const AccordionSummary = withStyles({
+  root: {
+    backgroundColor: "rgba(0, 0, 0, .03)",
+    borderBottom: "1px solid rgba(0, 0, 0, .125)",
+    marginBottom: -1,
+    minHeight: 56,
+    "&$expanded": {
+      minHeight: 56,
+    },
+  },
+  content: {
+    "&$expanded": {
+      margin: "12px 0",
+    },
+  },
+  expanded: {},
+})(MuiAccordionSummary);
+
+const AccordionDetails = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiAccordionDetails);
+
+export default function CustomizedAccordions() {
+  const [expanded, setExpanded] = React.useState("panel1");
+
+  const handleChange = (panel) => (event, newExpanded) => {
+    setExpanded(newExpanded ? panel : false);
+  };
+
   return (
     <div>
       <Helmet>
@@ -18,13 +71,21 @@ export default function FAQ() {
           content="FAQ frequently asked question"
         />
       </Helmet>
+
       <h1>FAQ: Frequently Asked Questions</h1>
-      <ul>
-        <li>
-          <p>
-            <b>Why does abettersettlement.com exist?</b>
-          </p>
-          <p>
+
+      <Accordion
+        square
+        expanded={expanded === "panel1"}
+        onChange={handleChange("panel1")}
+      >
+        <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+          <Typography>
+            <b>Why does ABetterSettlement.com exist?</b>
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
             We discovered that frequently car accident victims settle before
             they are fully recovered from whiplash and delay their recovery for
             a while because they never get the treatment due to insurance. We
@@ -36,63 +97,96 @@ export default function FAQ() {
             </a>
             ! It is full of good advice that the car insurance companies don't
             want you to know!
-          </p>
-        </li>
-        <li>
-          <p>
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion
+        square
+        expanded={expanded === "panel4"}
+        onChange={handleChange("panel4")}
+      >
+        <AccordionSummary aria-controls="panel4d-content" id="panel4d-header">
+          <Typography>
             <b>
               Why is my settlement from the insurance company amount different
               from what was estimated?
             </b>
-          </p>
-          <p>
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
             These settlement estimates provided via the calculator are
             conservative estimates on what a rational person should ask for
             using common accounting and financial analysis techniques. It is
             estimated 90% of all settlements for whiplash cases are too low.
-          </p>
-        </li>
-        <li>
-          <p>
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion
+        square
+        expanded={expanded === "panel5"}
+        onChange={handleChange("panel5")}
+      >
+        <AccordionSummary aria-controls="panel5d-content" id="panel5d-header">
+          <Typography>
             <b>What factors drives personal injury settlements?</b>
-          </p>
-          <p>
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
             Generally medical bills, a total car, lost income from doing rehab,
             pain and suffering, etc.
-          </p>
-        </li>
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
 
-        <li>
-          <p>
+      <Accordion
+        square
+        expanded={expanded === "panel4"}
+        onChange={handleChange("panel4")}
+      >
+        <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
+          <Typography>
             <b>
               Why should I use this data for my claim? You are not a lawyer.
             </b>
-          </p>
-          <p>
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
             Settling accident claims can be very arbitrary in nature. This is a
             tool to allow consumers and a fighting chance to make a full
             recovery from an accident. Large insurance companies already have
             tools like this, in fact they are significantly better using machine
             learning and artificial intelligence.
-          </p>
-        </li>
-
-        <li>
-          <p>
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        square
+        expanded={expanded === "panel5"}
+        onChange={handleChange("panel5")}
+      >
+        <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
+          <Typography>
             <b>
               Is "A Better Settlement" interested in working with lawyers, or
               investors?
             </b>
-          </p>
-          <p>
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
             Yes, "A Better Settlement" is interested in working with lawyers,
             medical professionals, or investors. Development has only just begun
             to build out a database of claim settlements for further analysis.
             Please feel free to contact us using the form on the About Page or
             at rb@abettersettlement.com.
-          </p>
-        </li>
-      </ul>
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
     </div>
   );
 }
